064caeff84916bc94df6e0985b626849dad652bc
zzfn <44751363+zzfn@users.noreply.github.com>
2022-08-18 13:50:41 +0800

    feat: favicon

--- site/App.tsx
+++ site/App.tsx
@@ -10,7 +10,7 @@ function App(): JSX.Element {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<CommonLayout />}>
-          <Route path="/" element={<div>欢迎使用</div>} />
+          <Route path="/" element={<div>欢迎使用👏</div>} />
           <Route path="/icon" element={<IconDemo/>} />
           {list.map((item) => (
             <Route
