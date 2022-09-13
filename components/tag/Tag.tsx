4f5d877d262ed03faef530c7c402df7f34712b14
orluna <44751363+zzfn@users.noreply.github.com>
2022-09-13 14:26:37 +0800

    feat: favicon

--- components/tag/Tag.tsx
+++ components/tag/Tag.tsx
@@ -5,10 +5,14 @@ type TagType = {
   children: React.ReactNode;
   size?: "small" | "default" | "medium" | "large";
   color?: string;
+  onClick?: () => void;
 };
-const Tag = ({ children, size = "default" }: TagType) => {
+const Tag = ({ children, size = "default", onClick }: TagType) => {
   return (
-    <span className={classNames("zzf-tag", `zzf-tag-size-${size}`)}>
+    <span
+      onClick={onClick}
+      className={classNames("zzf-tag", `zzf-tag-size-${size}`)}
+    >
       {children}
     </span>
   );
